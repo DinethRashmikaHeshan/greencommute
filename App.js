1e9c75700597bbefc2f12bec51cc5118f418e908
VKaushalya03 <vishwakaushalya203@gmail.com>
2024-10-09 10:03:10 +0530

    dd

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import React from "react";
-import AppNavigator from "./navigation/AppNavigator";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import CarbonFootprintMain from "./screens/CarbonFootprintMain";
+import ViewSavings from "./screens/ViewSavings";
+import CalculateFootprint from "./screens/CalculateFootprint";
+import SetGoals from "./screens/setGoals";
+
+const Stack = createStackNavigator();
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="CarbonFootprintMain">
+        <Stack.Screen
+          name="CarbonFootprintMain"
+          component={CarbonFootprintMain}
+        />
+        <Stack.Screen name="ViewSavings" component={ViewSavings} />
+        <Stack.Screen
+          name="CalculateFootprint"
+          component={CalculateFootprint}
+        />
+        <Stack.Screen name="SetGoals" component={SetGoals} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
